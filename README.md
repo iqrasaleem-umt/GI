e8638dda2c723ae15dfb5e7f45b2e4b777346237
iqra <fhsaleem8104@gmail.com>
2024-07-13 12:57:05 +0500

    claass01python

---
+++ README.md
@@ -0,0 +1,8 @@
+# inatallation
+* Anaconda
+* open in vscode
+* hello world
+* jupyter notebook 
+# virtual environment
+* required peckages
+* numpy pandas jupyter mypy
---
+++ class01.py
@@ -0,0 +1,16 @@
+# hello world
+
+print("Hey i am typing my first Hello world")
+print("i will try to cover modern python for Quarter 3")
+print("i will try to cover modern python for Quarter 3")
+print("i will try to cover modern python for Quarter 3")
+
+# string
+name="iqra"
+print(name)
+print(type(name))
+print(id(name))
+
+# string new method 
+name :str=299
+print(name)
